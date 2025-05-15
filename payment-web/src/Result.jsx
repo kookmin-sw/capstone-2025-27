ed22aacb577bc678d04a744965afc4853bda710b
gwangyeolpark <samjjng01@kookmin.ac.kr>
2025-05-15 21:14:55 +0900

    feat: React 결제 프론트엔드(payment-web) 추가

---
+++ payment-web/src/Result.jsx
@@ -0,0 +1,12 @@
+const Result = () => {
+  return (
+    <div style={{ padding: "40px", textAlign: "center" }}>
+      <h2>결제가 완료되었습니다.</h2>
+      <a href="yourapp://home">
+        <button>앱으로 돌아가기</button>
+      </a>
+    </div>
+  );
+};
+
+export default Result;
